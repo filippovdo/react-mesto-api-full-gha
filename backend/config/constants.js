a70922962e44ff1c33d9e449285e60eed76a195b
filippovdo <117165629+filippovdo@users.noreply.github.com>
2024-01-21 10:51:51 +0300

    Delete backend directory

--- backend/config/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports = Object.freeze({
-  urlRegular: /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/,
-});
--- backend/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-module.exports = (error, req, res, next) => {
-  const { statusCode = 500, message } = error;
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'Попробуйте позже, проблемы с сервером' : message,
-  });
-  next();
-};
